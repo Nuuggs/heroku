5ea80317cc6fa80fc336de4812974eca2a3a465e
Nuuggs <bryluke000@gmail.com>
2021-12-01 13:07:03 +0800

    added cats

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import express from 'express';
+import pg from 'pg';
 
-const PORT = 3004;
-
+const PORT = process.env.PORT || 3004;
 // Initialise Express
 const app = express();
+
+// initialize pool
+const { Pool } = pg;
+
+
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: 'bryluke000',
+    host: 'localhost',
+    database: 'bryluke000',
+    port: 5432,
+  };
+}
+const pool = new Pool(pgConnectionConfigs);
+
+
 
 app.set('view engine', 'ejs');
 
@@ -16,6 +44,17 @@ app.get('/bananas', (request, response) => {
   const data = { responseText };
 
   response.render('bananas', data);
+});
+
+app.get('/cats', (request, response) => {
+  console.log('request came in');
+  pool.query('SELECT * from cats').then((result) => {
+    console.log(result.rows[0].name);
+    response.send(result.rows);  
+  }).catch((error) => {
+    console.error('Error executing query', error.stack);
+    response.status(503).send(result.rows);
+  });;
 });
 
 app.listen(PORT);
